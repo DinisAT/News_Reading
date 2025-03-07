c57d6fa4d48b8d17eddebef0a49eec8cb67a8a40
Dinis Timoteo <115228493+DinisAT@users.noreply.github.com>
2025-03-07 10:35:55 +0100

    Temporary Update

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 import streamlit as st
 import pandas as pd
-import trafilatura
-from trafilatura import extract
-from trafilatura.settings import use_config
+import numpy as np
 
-st.title('Web Scraping App')
+st.markdown("""# This is a header -- hello
+## This is a sub header
+This is text""")
 
-def scraping(url: str):
-    # Solution for signal / thread error
-    config = use_config()
-    config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
-    down = trafilatura.fetch_url(url)
-    scraped_df = pd.DataFrame({'news': [extract(down, config=config)]})
-    return scraped_df
+df = pd.DataFrame({
+    'first column': list(range(1, 11)),
+    'second column': np.arange(10, 101, 10)
+})
 
-# Create text input for URL
-url = st.text_input('Enter URL to read:', )
+st.write("Hello World")
 
-if st.button('Read'):
-    # Show loading message
-    with st.spinner('Scraping content...'):
-        # Get scraped content
-        result = scraping(str(url))
-        # Display result
-        st.write('News Content:')
-        st.write(result['news'][0])
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+head_df = df.head(line_count)
+
+head_df
+
+
+# import trafilatura
+# from trafilatura import extract
+# from trafilatura.settings import use_config
+
+# st.title('Web Scraping App')
+
+# def scraping(url: str):
+#     # Solution for signal / thread error
+#     config = use_config()
+#     config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
+#     down = trafilatura.fetch_url(url)
+#     scraped_df = pd.DataFrame({'news': [extract(down, config=config)]})
+#     return scraped_df
+
+# # Create text input for URL
+# url = st.text_input('Enter URL to read:', )
+
+# if st.button('Read'):
+#     # Show loading message
+#     with st.spinner('Scraping content...'):
+#         # Get scraped content
+#         result = scraping(str(url))
+#         # Display result
+#         st.write('News Content:')
+#         st.write(result['news'][0])
